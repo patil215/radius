4203d1a6c7c091a862d18dec516b808fb75144e4
Ben Stobaugh <legoben1998@gmail.co>
2014-02-15 20:59:42 -0600

    Puuuuush

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,12 @@
  function sizeBox(){
     document.getElementById('search-term').size = document.getElementById('search-term').value.length + 3;}
+
+
+function map_initialize(latitude, longitude) {
+        var mapOptions = {
+          center: new google.maps.LatLng(latitude, longitude),
+          zoom: 14
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+}
