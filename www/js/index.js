146da62a2f9f9bffaa861b00caf8414fe1e5ecaa
Ben Stobaugh <legoben1998@gmail.co>
2014-02-15 23:04:50 -0600

    I can't Google Maps

--- www/js/index.js
+++ www/js/index.js
@@ -2,15 +2,6 @@
     document.getElementById('search-term').size = document.getElementById('search-term').value.length + 3;}
 
 
-function map_initialize(latitude, longitude) {
-        var mapOptions = {
-          center: new google.maps.LatLng(latitude, longitude),
-          zoom: 14
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-}
-
 function PlaceList(lat, lon, r) {
     this.lat = lat;
     this.lon = lon;
